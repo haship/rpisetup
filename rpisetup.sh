2273589bed074b13951614f15996efff6b36437b
haship <haship@gmail.com>
2021-11-06 20:27:13 +0900

    Update rpisetup.sh

--- rpisetup.sh
+++ rpisetup.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# カメラを有効化
+sudo raspi-config nonint do_camera 0
+
+# I2C有効化
+sudo raspi-config nonint do_i2c 0
+
+# ssh 有効化
+sudo raspi-config nonint do_ssh 0
+
+# ロケールの設定
+sudo raspi-config nonint do_change_locale ja_JP.UTF-8
+
+# タイムゾーン設定
+sudo raspi-config nonint do_change_timezone Asia/Tokyo
 
